abbc7285fcc8f9692dc1e14fd556727715a45d99
Alexandre Vrla <alexandre.vrla@gmail.com>
2021-12-13 20:37:24 +0100

    almost done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+bristol = Restaurant.create!(name: "Epicure", address: '75008 Paris', category: "french")
+wesh = Restaurant.create!(name: "Wesh", address: '75009 Paris', category: "french")
+unefois = Restaurant.create!(name: "Une fois", address: '75010 Paris', category: "belgian")
+cassecouille = Restaurant.create!(name: "Casse-couilles", address: '75008 Paris', category: "french")
+allezla = Restaurant.create!(name: "Allez-là", address: '75007 Paris', category: "french")
